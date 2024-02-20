f9bad680fa7bd5853208a1459bd3e68261749c68
Naeet <nathnaelmeonnen@gmail.com>
2024-02-20 02:49:06 -0800

    cocktail

--- sort.h
+++ sort.h
@@ -40,5 +40,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap_ints(int *a, int *b);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
